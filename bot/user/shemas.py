14d7c509f2309aadb71ecea529f3364cceceabde
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-05-22 14:40:46 +0300

    added shemas.py to user

---
+++ bot/user/shemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class TelegramIDModel(BaseModel):
+    telegram_id: int
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserModel(TelegramIDModel):
+    username: str | None
+    first_name: str | None
+    last_name: str | None
+
+
+class ServiceIDModel(BaseModel):
+    id: int
+
+
+class PaymentData(BaseModel):
+    user_id: int = Field(..., description='ID пользователя Telegram')
+    payment_id: str = Field(
+        ...,
+        max_length=255,
+        description='Уникальный ID платежа'
+        )
+    price: int = Field(..., description='Сумма платежа в рублях')
+    service_id: int = Field(..., description='ID товара')
